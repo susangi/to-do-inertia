2e40d33ecc70771cb74f24dbea2d745362c0b07d
Susangi Charuni <susangi@mylinex.com>
2024-07-10 14:54:48 +0530

    init project

---
+++ resources/js/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "@/Components/Navbar/NavbarComponent";
+
+function Dashboard() {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+}
+
+export default Dashboard;
